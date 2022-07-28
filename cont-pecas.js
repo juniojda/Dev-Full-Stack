f039039c83baf50650c80a03c262801a8529ae5e
juniojda <83515230+juniojda@users.noreply.github.com>
2022-07-28 14:15:27 -0300

    Update cont-pecas.js

--- cont-pecas.js
+++ cont-pecas.js
@@ -1,1 +1,8 @@
+// peça superior a 100g, pode cadastrar
+// capacidade de cada caixa, caso a lista seja superior 10 un, imprima: caixa não tem capacidade suficiente.
+// nome da peça inferior a 3 caracteres, informar mensagem de erro.
 
+var pesoPeca = 100
+var capacidadeCaixa = 10
+//----------------------------------------
+if 
