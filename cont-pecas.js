cafe8ee9f63e20552ae3d9f31f03a878eea126ed
juniojda <83515230+juniojda@users.noreply.github.com>
2022-08-02 21:57:47 -0300

    Update cont-pecas.js

--- cont-pecas.js
+++ cont-pecas.js
@@ -1,17 +1,23 @@
+let cadastro = "descricao"
+let peca = 1
+let pesoPeca = 100
+const limiteCaixa = 10
+let caracteres = cadastro.length
+//----------------------------------------
+
+if (cadastro.length < 4) {
+console.log ('ERRO DE CADASTRO')  
+}
+else {
+  console.log ('CADASTRAR PEÇA')
+}
+if (peca > pesoPeca) {
+console.log ('PESO LIBERADO PARA CADASTRO')  
+}
+else {
+  console.log ('PESO NÃO PERMITIDO PARA CADASTRO')
+}
+
 // peça superior a 100g, pode cadastrar
 // capacidade de cada caixa, caso a lista seja superior 10 un, imprima: caixa não tem capacidade suficiente.
 // nome da peça inferior a 3 caracteres, informar mensagem de erro.
-
-const pesoPeca = 100g
-const capacidadeCaixa = 10
-const caracteres = 3 
-//----------------------------------------
-
-if (pesoPeca > 100)
-{
-  console.log ('CADASTRAR PEÇA');
-}
-else 
-{
-  console.log ('CADASTRO NÃO PERMITIDO');
-} 
