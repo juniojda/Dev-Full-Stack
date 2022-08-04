4221a33477085d2c8c7a615550d61cfa78ac8a11
Junio Jose Dutra de Almeida <junioda@gmail.com>
2022-08-03 21:52:50 -0300

    arquivos nao sobem

--- cont-pecas.js
+++ cont-pecas.js
@@ -1,5 +1,6 @@
-let cadastro = "descricao"
-let peca = 1
+let cadastro = "des"
+const pecaUn = 1
+let quantPeca = 11
 let pesoPeca = 100
 const limiteCaixa = 10
 let caracteres = cadastro.length
@@ -11,11 +12,16 @@ console.log ('ERRO DE CADASTRO')
 else {
   console.log ('CADASTRAR PEÇA')
 }
-if (peca > pesoPeca) {
+if (pecaUn > pesoPeca) {
 console.log ('PESO LIBERADO PARA CADASTRO')  
 }
 else {
   console.log ('PESO NÃO PERMITIDO PARA CADASTRO')
+}
+if (quantPeca > limiteCaixa) {
+  console.log ('CAIXA NÃO TEM CAPACIDADE SUFICIENTE')
+} else {
+  console.log ('CAPACIDADE DA CAIXA DENTRO DO PERMITIDO')
 }
 
 // peça superior a 100g, pode cadastrar
