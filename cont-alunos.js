6ab47b3934fc6fb2cd7076a8c4c4ebd0c910d32c
Junio Jose Dutra de Almeida <junioda@gmail.com>
2022-08-04 23:02:16 -0300

    subir remoto

---
+++ cont-alunos.js
@@ -0,0 +1,23 @@
+let nAlunos = 10
+
+for(let contador =0; contador < nAluno; contador++)
+{
+    console.log(contador)
+
+    if (contador = 0 ) 
+    {
+    console.log ('zero')    
+    }
+    if (contador % 2 == 0) 
+    {
+    console.log ('impar')    
+    } 
+    else 
+    {
+    console.log ('par')   
+    }
+}
+
+//Se o numero for par , escreva par, e o numero correspondente
+//Se o numero for impar, escreva impar e o numero correspondete
+//Se o numero for zero, e o numero correspondente 
